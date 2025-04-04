a76657243f62a78a8bcd8088c41a812e0b8c5930
Dyzu15 <tacpack10@gmail.com>
2025-04-04 23:46:38 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,4 @@
-// === Firebase Setup ===
-const firebaseConfig = {
-  apiKey: "AIzaSyCVZSmhs1Bz8pHa6e5w-0p1lGsdGgFqWWI",
-  authDomain: "manhwa-tracker-701f9.firebaseapp.com",
-  projectId: "manhwa-tracker-701f9",
-  storageBucket: "manhwa-tracker-701f9.firebasestorage.app",
-  messagingSenderId: "775288113585",
-  appId: "1:775288113585:web:5094032a41980a662e1590",
-  measurementId: "G-LFESV3X9JX"
-};
 
-firebase.initializeApp(firebaseConfig);
 const db = firebase.firestore();
 
 // === SPA Navigation ===
