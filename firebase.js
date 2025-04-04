dc40e56949b99d8dd24b9f5db68b3e8b404031f7
Dyzu15 <tacpack10@gmail.com>
2025-04-05 00:41:35 +0100

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,13 +1,18 @@
 const firebaseConfig = {
-    apiKey: "AIzaSyCVZSmhs1Bz8pHa6e5w-0p1lGsdGgFqWWI",
-    authDomain: "manhwa-tracker-701f9.firebaseapp.com",
-    projectId: "manhwa-tracker-701f9",
-    storageBucket: "manhwa-tracker-701f9.firebasestorage.app",
-    messagingSenderId: "775288113585",
-    appId: "1:775288113585:web:5094032a41980a662e1590",
-    measurementId: "G-LFESV3X9JX"
-  };
-  
-  firebase.initializeApp(firebaseConfig);
-  const db = firebase.firestore();
- window.db.collection('manhwa').get()
+  apiKey: "AIzaSyCVZSmhs1Bz8pHa6e5w-0p1lGsdGgFqWWI",
+  authDomain: "manhwa-tracker-701f9.firebaseapp.com",
+  projectId: "manhwa-tracker-701f9",
+  storageBucket: "manhwa-tracker-701f9.firebasestorage.app",
+  messagingSenderId: "775288113585",
+  appId: "1:775288113585:web:5094032a41980a662e1590",
+  measurementId: "G-LFESV3X9JX"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+// Get Firestore
+const db = firebase.firestore();
+
+// Expose `db` globally so other files like script.js can use it
+window.db = db;
